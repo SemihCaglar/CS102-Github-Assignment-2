a1fa8318e62693c17debe01555687fe26707dfad
Semih Çağlar <semihcaglar28@gmail.com>
2025-02-05 18:26:22 +0300

    Fix some bugs Menu is now working without problem

--- assignment2.java
+++ assignment2.java
@@ -17,25 +17,24 @@ public class assignment2 {
             int random = randn.nextInt(101);
             array[i] = random;
         }
+        System.out.println("Array: " + Arrays.toString(array));
         
         int choice;
         do{
             displayMenu();
-            choice = readAChoice();
+            choice = readAChoice(in);
             switch(choice){
                 case 1:
-                    System.out.println("Array's minimum: "+findMinOfArray(array));
+                    findMinOfArray(array);
                     break;
                 case 2:
-                    System.out.println("Array's maximum: "+findMaxOfArray(array));
+                    findMaxOfArray(array);
                     break;
                 case 3:
-                    int average = findAverage(array);
-                    System.out.println("The average of the array is " + average);
-                    displayDifferences(array, average);
+                    findAverageAndDisplayDifferences(array);
                     break;
                 case 4:
-                //will added method
+                    findSumOfOddAndEvenIndexes(array);
                     break;
             }
         }while(choice != 5);
@@ -47,64 +46,84 @@ public class assignment2 {
         //the menu option names can be changed
         System.out.println("1- Find the array's minimum.");
         System.out.println("2- Find the array's maximum.");
-        System.out.println("3- Find the avarage value of the array.");
-        System.out.println("4- Find the sum of elements with odd- and even-numbered indexes.");
+        System.out.println("3- Find the average value of the array.");
+        System.out.println("4- Find the sum of elements with odd and even numbered indexes.");
         System.out.println("5- Exit");
     }
 
     // reading choice from user
-    public static int readAChoice()
+    public static int readAChoice(Scanner in)
     {
-        Scanner sc = new Scanner(System.in);
         System.out.print("Choice: ");
-        return sc.nextInt();
+        return in.nextInt();
     }
 
     //finding the array's minimum
-    public static int findMinOfArray(int[] array)
+    public static void findMinOfArray(int[] array)
     {
-        int min=array[0];
-        for(int num:array){
-            if(num<min)
-                min=num;
-            
+        int min = array[0];
+        for(int num : array){
+            if(num < min)
+                min = num;
         }
-        return min;
+        System.out.println("Array's minimum: " + min);
     }
 
     //finding the array's maximum
-    public static int findMaxOfArray(int[] array)
+    public static void findMaxOfArray(int[] array)
     {
-        int max=array[0];
-        for(int num:array){
-            if(num>max)
-                max=num;
+        int max = array[0];
+        for(int num : array){
+            if(num > max)
+                max = num;
         }
-        return max;
+        System.out.println("Array's maximum: " + max);
     }
     
-    //finding the average and display 
-    public static int findAverage(int[] array)
+    //finding the average and display differences
+    public static void findAverageAndDisplayDifferences(int[] array)
     {
         int sum = 0;
-        int average;
-        for(int number: array)
+        for(int number : array)
         {
             sum += number;
         }
-        average = sum / array.length;
-        return average;
-    }
-    
-    //prints differences between elements of array and the average
-    public static void displayDifferences(int[] array, int average)
-    {
-        int[] arrayOfDifferences = new int[array.length];
-        for(int i =0 ; i < array.length; i ++)
+        double average = (double) sum / array.length;
+        System.out.println("The average of the array is " + average);
+
+        double[] arrayOfDifferences = new double[array.length];
+        for(int i = 0; i < array.length; i++)
         {
             arrayOfDifferences[i] = array[i] - average;
         }
-        System.out.println("Differences between elements of array and the average is " + Arrays.toString(arrayOfDifferences));
+        System.out.print("Differences between elements of array and the average is [");
+        for(int i = 0; i < arrayOfDifferences.length; i++)
+        {
+            System.out.printf("%.2f", arrayOfDifferences[i]);
+            if(i < arrayOfDifferences.length - 1)
+            {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
     }
 
+    public static void findSumOfOddAndEvenIndexes(int[] array)
+    {
+        int sumOfOddIndexes = 0;
+        int sumOfEvenIndexes = 0;
+        for(int i = 0; i < array.length; i++)
+        {
+            if(i % 2 == 0)
+            {
+                sumOfEvenIndexes += array[i];
+            }
+            else
+            {
+                sumOfOddIndexes += array[i];
+            }
+        }
+        System.out.println("Sum of odd indexes: " + sumOfOddIndexes);
+        System.out.println("Sum of even indexes: " + sumOfEvenIndexes);
+    }
 }
